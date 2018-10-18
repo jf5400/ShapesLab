b60a3950b1c9c9b3d349524b4f9e471c7ac9330f
Astor730 <42044772+Astor730@users.noreply.github.com>
2018-10-17 21:37:48 -0400

    Actual Runner

---
+++ src/Runner.java
@@ -0,0 +1,16 @@
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        Shape shape1 = new Circle(6);
+        Shape shape2 = new ETriangle(8);
+        Shape shape3 = new Rectangle(9,4);
+        Shape[] shapes = {shape1, shape2,shape3};
+        for (Shape cur : shapes)
+        {
+            System.out.println(cur);
+            System.out.println("My area is " + cur.getArea());
+            System.out.println("My perimeter is " + cur.getPerimeter());
+        }
+    }
+}
