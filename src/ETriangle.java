24c87c19b89dd4b4f36a6fc08dee09d7f47ccec2
BT_1N3_15 <jfreund5400@bths.edu>
2018-10-18 14:36:23 -0400

    ITriangle

--- src/ETriangle.java
+++ src/ETriangle.java
@@ -1,16 +1,18 @@
 public class ETriangle extends Shape
 {
+    //identifies the side of the triangle
     private int side;
-
     public ETriangle(int side)
     {
         super("ETriangle");
         this.side=side;
     }
+    //Calculates and returns the area of the triangle
     @Override
     public double getArea(){
         return ((Math.pow(3,.5)*.25)*side*side);
     }
+    //Calculates and returns the perimeter of the triangle
     @Override
     public double getPerimeter(){
         return side*3;
